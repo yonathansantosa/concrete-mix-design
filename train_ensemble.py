6ed96454459b1c90111da04ee78f056c06c985de
yonathansantosa <yonathansantosa@gmail.com>
2019-12-09 19:49:08 +0100

    split training bootstrap and ensemble

--- train_ensemble.py
+++ train_ensemble.py
@@ -20,12 +20,9 @@ parser = argparse.ArgumentParser(
     description='Conditional Text Generation: Train Discriminator'
 )
 
-parser.add_argument('--maxepoch', default=1000, help='maximum iteration (default=1000)')
-parser.add_argument('--agr_maxepoch', default=1000, help='maximum aggregate iteration (default=1000)')
-parser.add_argument('--lr', default=0.1, help='learing rate (default=0.1)')
-parser.add_argument('--agr_lr', default=0.1, help='aggregate learing rate (default=0.1)')
+parser.add_argument('--maxepoch', default=1000, help='maximum aggregate iteration (default=1000)')
+parser.add_argument('--lr', default=0.1, help='aggregate learing rate (default=0.1)')
 parser.add_argument('--batch', default=32, help='minibatch size (default=32)')
-parser.add_argument('--agr_batch', default=32, help='minibatch size (default=32)')
 parser.add_argument('--b', default=2, help='number of bag (default=2)')
 parser.add_argument('--seed', default=2, help='number of bag (default=2)')
 parser.add_argument('--bsize', default=100, help='number of bag size sample (default=100)')
@@ -59,56 +56,27 @@ indices = list(range(data_size))
 split = int(np.floor(validation_split * data_size))
 np.random.shuffle(indices)
 
-for b in range(b_max):
-    # if args.wandb: wandb.init(project="concrete-mix-design")
+# Creating PT data samplers and loaders:
+train_indices, val_indices = indices[:split], indices[split:]
+np.random.shuffle(train_indices)
+np.random.shuffle(val_indices)
+train_sampler = SubsetRandomSampler(train_indices)
+valid_sampler = SubsetRandomSampler(val_indices)
+train_loader = DataLoader(data, batch_size=batch_size, sampler=train_sampler)
+validation_loader = DataLoader(data, batch_size=val_batch_size, sampler=valid_sampler)
 
-    # Creating PT data samplers and loaders:
-    train_indices, val_indices = indices[:split], indices[split:]
-    train_indices = np.random.choice(train_indices, size=(int(args.bsize)))
-    np.random.shuffle(val_indices)
-    train_sampler = SubsetRandomSampler(train_indices)
-    valid_sampler = SubsetRandomSampler(val_indices)
-    train_loader = DataLoader(data, batch_size=batch_size, sampler=train_sampler)
-    validation_loader = DataLoader(data, batch_size=val_batch_size, sampler=valid_sampler)
+# Hyperparameter
+model = cnn()
+model.to(device)
+learning_rate = float(args.lr)
+max_epoch = int(args.maxepoch)
+momentum=0.1
 
-
-    # Hyperparameter
-    learning_rate = float(args.lr)
-    model = cnn()
-    model.to(device)
-    max_epoch = int(args.maxepoch)
-    momentum=0.1
-
-    # if args.wandb: wandb.watch(model)
-    optimizer = optim.SGD(model.parameters(), lr=learning_rate, momentum=momentum, nesterov=True)
-    criterion = nn.MSELoss()
-
-    for epoch in trange(0, max_epoch, total=max_epoch, initial=0):
-        model.train()
-        for it, (X, y) in enumerate(train_loader):
-            model.zero_grad()
-            inputs = Variable(X, requires_grad=True).to(device)
-            output = model.forward(inputs)
-            target = Variable(y.unsqueeze(1)).to(device)
-            loss = criterion(output, target)
-            loss.backward()
-            
-            # if args.wandb: if it==0: wandb.log({"Train Loss": loss.data.cpu().item()}, step=epoch)
-
-            optimizer.step()
-            optimizer.zero_grad()
-
-        model.eval()
-        val_loss = 0.
-        for it, (X, y) in enumerate(train_loader):
-            model.zero_grad()
-            inputs = Variable(X, requires_grad=True).to(device)
-            output = model.forward(inputs)
-            target = Variable(y.unsqueeze(1)).to(device)
-            val_loss += F.mse_loss(output, target, reduction='sum').sum().data.cpu().item()
-        # if args.wandb: wandb.log({"Validation Loss": val_loss/len(val_indices)}, step=epoch)
-
-    torch.save(model.state_dict(), f'{saved_model_path}/model-{b}.pth')
+# Creating dataset split
+data_size = len(data)
+indices = list(range(data_size))
+split = int(np.floor(validation_split * data_size))
+np.random.shuffle(indices)
 
 models = []
 models_param = []
@@ -137,13 +105,13 @@ if args.trainable_bag:
 else:
     optimizer = optim.SGD(list(aggregate.parameters()), lr=learning_rate, momentum=momentum, nesterov=True)
 
-if args.wandb: wandb.init(project="concrete-mix-design")
+if args.wandb: wandb.init(project="concrete-mix-design", name='aggregate')
 if args.wandb: wandb.watch(aggregate)
 
-agr_learning_rate = float(args.agr_lr)
-agr_max_epoch = int(args.agr_maxepoch)
 
-for epoch in trange(0, agr_max_epoch, total=agr_max_epoch, initial=0):
+criterion = nn.MSELoss()
+
+for epoch in trange(0, max_epoch, total=max_epoch, initial=0):
     aggregate.train()
     for it, (X, y) in enumerate(train_loader):
         aggregate.zero_grad()
