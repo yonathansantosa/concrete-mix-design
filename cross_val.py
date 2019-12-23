5df2628a8f35be0caca8229b39dc74c1adb59d2d
yonathansantosa <yonathansantosa@gmail.com>
2019-12-23 22:17:41 +0100

    fix missing brackets

--- cross_val.py
+++ cross_val.py
@@ -67,7 +67,7 @@ def cross_val(
                 output = model.forward(inputs)
                 target = Variable(y.unsqueeze(1)).to(device)
                 val_loss += F.mse_loss(output, target, reduction='sum').data.cpu().item()
-            validation_loss[epoch] = float(val_loss/(len(validation_cross_indices))
+            validation_loss[epoch] = float(val_loss/(len(validation_cross_indices)))
 
         if validation_loss[-1] < val_loss_min:
             val_loss_min = validation_loss[-1]
