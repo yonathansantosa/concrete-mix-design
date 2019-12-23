4509cb2a407234a13d3ecb32c0d761a7bed41009
yonathansantosa <yonathansantosa@gmail.com>
2019-12-23 22:11:17 +0100

    add train printing

--- train_ensemble.py
+++ train_ensemble.py
@@ -141,7 +141,7 @@ for epoch in trange(0, max_epoch, total=max_epoch, initial=0):
         # nn.utils.clip_grad_value_(aggregate.parameters(), 1)
         if args.wandb and it==0: 
             wandb.log({"Aggregate Train Loss": loss.data.cpu().item()}, step=epoch)
-        # if it==0 and not args.quiet: tqdm.write(f'{models_out[0].detach().cpu().numpy()} =====> {output[0].data.cpu().item()} || {y[0].data.cpu().item()}')
+        if it==0 and not args.quiet: tqdm.write(f'{models_out[0].detach().cpu().numpy()} =====> {output[0].data.cpu().item()} || {y[0].data.cpu().item()}')
         optimizer.step()
         optimizer.zero_grad()
 
@@ -158,12 +158,12 @@ for epoch in trange(0, max_epoch, total=max_epoch, initial=0):
         target = Variable(y.unsqueeze(1)).to(device)
         val_loss += F.mse_loss(output, target, reduction='sum').sum().data.cpu().item()/len(test_indices)
 
-        if it == 0 and not args.quiet and epoch % 5 == 0:
-            X_test = (torch.tensor([139.6,209.4,0.0,192.0,0.0,1047.0,806.9,3]).to(device) - data.X_mean)/data.X_std
-            y_test = torch.tensor([[8.06]])
-            inputs_test = Variable(X_test)
-            models_out, output = aggregate.forward(inputs_test)
-            tqdm.write(f'{models_out[0].detach().cpu().numpy()} =====> {output[0].data.cpu().item():.2f} || {y_test[0].data.cpu().item():.2f}')
+        # if it == 0 and not args.quiet and epoch % 5 == 0:
+        #     X_test = (torch.tensor([139.6,209.4,0.0,192.0,0.0,1047.0,806.9,3]).to(device) - data.X_mean)/data.X_std
+        #     y_test = torch.tensor([[8.06]])
+        #     inputs_test = Variable(X_test)
+        #     models_out, output = aggregate.forward(inputs_test)
+        #     tqdm.write(f'{models_out[0].detach().cpu().numpy()} =====> {output[0].data.cpu().item():.2f} || {y_test[0].data.cpu().item():.2f}')
             # tqdm.write(f'{float(output[0].cpu().data)} ==> {float(target[0].cpu().data)}')
     if args.wandb:
         wandb.log({"Aggregate Validation Loss": val_loss}, step=epoch)
