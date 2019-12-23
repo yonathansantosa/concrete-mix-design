fix data normalization

train_ensemble.py
@@ -158,9 +158,9 @@ for epoch in trange(0, max_epoch, total=max_epoch, initial=0):
         val_loss += F.mse_loss(output, target, reduction='sum').sum().data.cpu().item()/len(test_indices)
 
         if it == 0 and not args.quiet:
-            X_test = torch.tensor([139.6,209.4,0.0,192.0,0.0,1047.0,806.9,3])
+            X_test = (torch.tensor([139.6,209.4,0.0,192.0,0.0,1047.0,806.9,3]).to(device) - data.X_mean)/data.X_std
             y_test = torch.tensor([[8.06]])
-            inputs_test = Variable(X_test).to(device)
+            inputs_test = Variable(X_test)
             models_out, output = aggregate.forward(inputs_test)
             tqdm.write(f'{models_out[0].detach().cpu().numpy()} =====> {output[0].data.cpu().item()} || {y_test[0].data.cpu().item()}')
             # tqdm.write(f'{float(output[0].cpu().data)} ==> {float(target[0].cpu().data)}')